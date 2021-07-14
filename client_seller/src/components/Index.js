3c4395350d3cfc40ee3bb4dd44a97f50c6dd51f9
j-mello <mello.julien@gmail.com>
2021-07-14 11:59:37 +0200

    create List component

--- client_seller/src/components/Index.js
+++ client_seller/src/components/Index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import List from './List';
 
 
 function Index() {
+    const seller = JSON.parse(localStorage.getItem('seller'));
+    console.log(seller);
     return (
         <div>
             <h1>Site marchand</h1>
+            { seller == null && 
+                <List></List> }
         </div>
     )
 }
