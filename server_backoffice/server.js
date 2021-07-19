d56a2c028de3b3a4c36b7c6119c018267fe77e2b
thomas.chatel <thomas.chatel.acversailles@gmail.com>
2021-07-19 21:54:40 +0200

    add conversion rate backoffice api endpoints

--- server_backoffice/server.js
+++ server_backoffice/server.js
@@ -3,6 +3,7 @@ const UserRouter = require("./routes/UserRouter");
 const SecurityRouter = require("./routes/SecurityRouter");
 const SellerRouter = require("./routes/SellerRouter");
 const CredentialsRouter = require("./routes/CredentialsRouter");
+const ConversionRateRouter = require("./routes/ConversionRateRouter");
 const mustacheExpress = require("mustache-express");
 const migrate = require("./lib/sequalizeloader");
 const cors = require('cors');
@@ -23,5 +24,6 @@ app.use("/users", UserRouter);
 app.use("/sellers", SellerRouter);
 app.use("/credentials", CredentialsRouter);
 app.use("/auth", SecurityRouter);
+app.use("/conversionRate", ConversionRateRouter);
 
 app.listen(process.env.PORT || 3000, () => console.log("server_backoffice listening"));
